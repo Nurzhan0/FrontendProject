495b9e0926e103c2e74faa0dc1e143285b25b797
Nurzhan0 <nurzhaan0@gmail.com>
2022-10-17 01:58:48 +0600

    dark mode header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,25 @@
-import React, { useState } from "react";
+import { createContext, useState } from "react";
 import { Link } from "react-router-dom"
 import "./navbar.css"
 import {FaBars} from "react-icons/fa"
 import {ImCross} from "react-icons/im"
+import ReactSwitch from "react-switch";
+
+export const ThemeContext = createContext(null)
 
 const Navbar = () => {
     const[Mobile, setMobile] = useState(false)
+    const[theme, setTheme] = useState("light") 
+    const toggleTheme = () => {
+        setTheme((curr) => (curr == "light" ? "dark" : "light"));
+    };
     
     return(
-        <nav className="navbar">
-                <h3 className="logo">HandGesture</h3>
+        <ThemeContext.Provider value={{theme, toggleTheme}}>
+        <nav className="navbar" id = {theme}>
+                <h3 className="logo" id = {theme}>HandGesture</h3>
  
-                <ul className =  {Mobile ? "nav-links-mobile" : "nav-links"} onClick = {() => setMobile}>
+                <ul className =  {Mobile ? "nav-links-mobile" : "nav-links"} onClick = {() => setMobile} id = {theme}>
                     <Link to="/">
                         <li>Home</li>
                         </Link>
@@ -21,12 +29,16 @@ const Navbar = () => {
                     <Link to="/contact">
                        <li>Contact</li>
                     </Link>
+                    <li><b>Change theme</b></li>
                 </ul>
+                <div className="switch">
+                <ReactSwitch onChange={toggleTheme} checked={theme==="dark"} />
+                </div>
                 <button className="mobile-menu-icon" onClick={() => setMobile(!Mobile)}>
                     {Mobile? <ImCross/> : <FaBars/>}
                 </button>
         </nav>
-        
+        </ThemeContext.Provider>
     )
 }
 
